6891ced901541bd4c7a9685b87126cf6e77c755a
greensabre1 <daniiltodnem@gmail.com>
2023-09-21 15:38:43 +0300

    Some fixes

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -20,7 +20,8 @@ describe('Issue deletion', () => {
     })
     cy.get('div').contains('Delete issue').click()
     //asserts modal is gone and length is deducted by 1
-
+    cy.get('[data-testid="modal:confirm"]').should('not.exist');
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`)
     cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', '1').within(() => {
       cy.get('[data-testid="list-issue"]')
@@ -39,7 +40,7 @@ describe('Cancel issue deletion', () => {
     cy.visit(url + '/board')
     })
 })
-  it.only('Cancel deletion', () => {
+  it('Cancel deletion', () => {
         cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', '1').within(() => {
         cy.get('[data-testid="list-issue"]')
               .should('have.length', '4')
@@ -56,6 +57,7 @@ describe('Cancel issue deletion', () => {
     cy.get('[data-testid="modal:issue-details"]').should('be.visible')
     //clicks on X button to close issue modal
     cy.get('[data-testid="icon:close"]').eq(0).click()
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
     //asserts that we back on the board page, backlog is visible length hasnt been changed
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`)
     cy.wait(6000)
@@ -66,70 +68,4 @@ describe('Cancel issue deletion', () => {
   })  
  })
 
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*   it('Modal assertion, clicks trash icon, clicks confirm deletion', () => {
-        cy.get(['data-testid="modal:issue-details"']).should('be.visible')
-        cy.get('data-testid="icon:trash"').click()
-        cy.get('div').contains('Delete issue').click()
-        //asserts confirmation modal is gone
-        cy.get('[data-testid="modal:confirm"]').should('not.be.visible')
-    });
-    it('Asserts issue deletion by checking list issue length', () => {
-        cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', '1').within(() => {
-        cy.get('[data-testid="list-issue"]')
-              .should('have.length', '3')
-     })
- describe('Cancel the deletion of the issue and assert its still there', () => {
-    it('This test opens first issue on the board', () => {
-        cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', '1').within(() => {
-          cy.get('[data-testid="list-issue"]')
-              .should('have.length', '4')
-              .first()
-              .find('p')
-              .click()
-        });
-      });
-    it('Modal assertion, clicks trash icon, clicks canel', () => {
-        cy.get(['data-testid="modal:issue-details"']).should('be.visible')
-        cy.get('data-testid="icon:trash"').click()
-        cy.get('div').contains('Cancel').click()
-        //asserts confirmation modal is gone
-        cy.get('[data-testid="modal:confirm"]').should('not.be.visible')
-        //asserts that issue modal is visible
-        cy.get('[data-testid="modal:issue-details"]').should('be.visible')
-        //clicks on X button to close issue modal
-        cy.get('[data-testid="icon:close"]').click()
-        //asserts that we back on the board page
-        cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`)
-    });
-    it('Asserts ithat issue has not been deleted by checking list issue length', () => {
-        cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', '1').within(() => {
-        cy.get('[data-testid="list-issue"]')
-              .should('have.length', '4')
-  });
-})
-})*/
- 
   
